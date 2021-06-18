ddcbd71700aad417743bbd0658be37d6b162aaa1
Rennan Silva Costa <rennansilvacosta@hotmail.com>
2021-06-17 21:40:04 -0300

    fix repo

--- aula2/src/model/Cadastro.java
+++ aula2/src/model/Cadastro.java
@@ -2,58 +2,58 @@ package model;
 
 public class Cadastro {
 
-	private Integer id;
-	private String nome;
-	private Long telefone;
-	private String email;
+    private Integer id;
+    private String nome;
+    private Long telefone;
+    private String email;
 
-	public Cadastro() {
+    public Cadastro() {
 
-	}
+    }
 
-	public Cadastro(Integer id, String nome, Long telefone, String email) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.telefone = telefone;
-		this.email = email;
-	}
+    public Cadastro(Integer id, String nome, Long telefone, String email) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public Long getTelefone() {
-		return telefone;
-	}
+    public Long getTelefone() {
+        return telefone;
+    }
 
-	public void setTelefone(Long telefone) {
-		this.telefone = telefone;
-	}
+    public void setTelefone(Long telefone) {
+        this.telefone = telefone;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	@Override
-	public String toString() {
-		return "\n id: " + id + "\n nome: " + nome + "\n telefone: " + telefone + "\n email: " + email;
-	}
+    @Override
+    public String toString() {
+        return "\n id: " + id + "\n nome: " + nome + "\n telefone: " + telefone + "\n email: " + email;
+    }
 
 }
